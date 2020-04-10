c5d538bd56f13681882b402e60667a559b9bf7db
Hakan Bilgin <hbi99@hotmail.com>
2020-04-10 18:13:18 +0200

    back and forward tool buttons

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,15 @@ const preferences = {
 	init() {
 		// fast references
 		this.activeSection =
-		this.mainMenu = window.find(`content > section[data-view="main"]`);
+		this.mainMenu = window.find(`content > section[data-view="Main"]`);
+		this.el = {
+			btnPrev: window.find("[data-click='history-go'][data-arg='-1']"),
+			btnNext: window.find("[data-click='history-go'][data-arg='1']"),
+		};
+		this.history = new window.History;
+		this.history.push({view: "Main"});
+		this.setViewState();
 		
-		this.dispatch({type: "main-menu"});
-
 		// tmp
 		//window.find(`section[data-view="main"] .section:nth(0)`).trigger("click");
 		//window.find(`section[data-view="Desktop"] .tab-row_ > div:nth(1)`).trigger("click");
@@ -14,42 +19,38 @@ const preferences = {
 		//window.find(`section[data-view="main"] .section:nth(17)`).trigger("click");
 	},
 	dispatch(event) {
-		let el,
+		let self = preferences,
+			state,
 			section,
 			workspace,
+			view,
 			index,
 			value,
-			flag;
+			flag,
+			el;
 		switch (event.type) {
-			case "set-highlight-color":
-			case "set-sidebar-icon-size":
-				console.log(event);
+			case "history-go":
+				if (event.arg === "-1") self.history.goBack();
+				else self.history.goForward();
+
+				// update view state
+				self.setViewState();
 				break;
 			case "main-menu":
-				this.activeSection.removeClass("active");
-				this.mainMenu.addClass("active");
-		
-				window.body.css({
-					width: this.mainMenu.width() +"px",
-					height: this.mainMenu.height() +"px",
-				});
+				self.history.push({view: "Main"});
+				self.setViewState();
 				break;
 			case "select-section":
 				el = $(event.target);
 				if (!el.hasClass("section")) el = el.parents(".section");
 
-				value = el.find(".name").text();
-				section = window.find(`section[data-view="${value}"]`);
-
-				window.body.css({
-					width: section.width() +"px",
-					height: section.height() +"px",
-				});
-				
-				this.mainMenu.removeClass("active");
-				this.activeSection = section.addClass("active");
-
-				this.renderView({view: value});
+				view = el.find(".name").text();
+				self.history.push({ view });
+				self.setViewState();
+				break;
+			case "set-highlight-color":
+			case "set-sidebar-icon-size":
+				console.log(event);
 				break;
 			case "select-folder-icon-color":
 				el = $(event.target);
@@ -69,11 +70,15 @@ const preferences = {
 				el.parent().find(".active").removeClass("active");
 				el.addClass("active");
 
-				this.renderView({
-					view: "Desktop - List",
-					folder: el.find("span.name").text(),
-					type: el.attr("data-type")
+				section = el.parents("section[data-view]");
+				name = el.find("span.name").text();
+				window.render({
+					template: "bg-list",
+					match: `//Data/Desktop/i[@name="${name}"]`,
+					target: section.find(".list").scrollTop(0)
 				});
+
+				section.toggleClass("wide-wp", el.attr("data-type") !== "wide");
 				break;
 			case "select-bg-item":
 				el = $(event.target);
@@ -100,50 +105,64 @@ const preferences = {
 				break;
 		}
 	},
-	async renderView(event) {
-		let view = window.find(`section[data-view="${event.view}"]`);
+	async setViewState() {
+		let state = this.history.current,
+			section = window.find(`section[data-view="${state.view}"]`),
+			width = this.mainMenu.width(),
+			height = this.mainMenu.height();
+		
+		// toolbar UI update
+		this.el.btnPrev.toggleClass("tool-disabled_", this.history.canGoBack);
+		this.el.btnNext.toggleClass("tool-disabled_", this.history.canGoForward);
 
-		switch (event.view) {
-			case "Desktop":
-				if (view.find(".tree .tree-item").length) return;
+		if (state.view !== "Main") {
+			width = section.width();
+			height = section.height();
+		}
 
-				let shell = await defiant.shell("ws -a wallpaper"),
-					isWide;
-				shell.result.map((item, index) => {
-					if (index === 0 && item.wide) {
-						isWide = item.value;
-					}
-					window.body.find(`.workspace[data-id="${item.name}"] div`).attr({
-						style: item.value
-					});
-				});
-				if (isWide) {
-					view.find(".reel").addClass("wide");
-					view.find(".workspace-wide.active").find("div").attr({style: isWide});
-				}
-
-				window.render({
-					template: "bg-tree",
-					match: `//Data/Desktop`,
-					target: view.find(".tree")
-				});
-
-				window.render({
-					template: "bg-list",
-					match: `//Data/Desktop/i[@name="Ultra-Wide"]`,
-					target: view.find(".list")
-				});
+		switch (state.view) {
+			case "Main":
 				break;
-			case "Desktop - List":
-				window.render({
-					template: "bg-list",
-					match: `//Data/Desktop/i[@name="${event.folder}"]`,
-					target: view.find(".list").scrollTop(0)
-				});
+			case "Desktop":
+				// ignore if already rendered
+				if (!section.find(".tree .tree-item").length) {
+					let shell = await defiant.shell("ws -a wallpaper"),
+						isWide;
+					shell.result.map((item, index) => {
+						if (index === 0 && item.wide) {
+							isWide = item.value;
+						}
+						window.body.find(`.workspace[data-id="${item.name}"] div`).attr({
+							style: item.value
+						});
+					});
+					if (isWide) {
+						view.find(".reel").addClass("wide");
+						view.find(".workspace-wide.active").find("div").attr({style: isWide});
+					}
 
-				view.toggleClass("wide-wp", event.type !== "wide");
+					window.render({
+						template: "bg-tree",
+						match: `//Data/Desktop`,
+						target: section.find(".tree")
+					});
+
+					window.render({
+						template: "bg-list",
+						match: `//Data/Desktop/i[@name="Ultra-Wide"]`,
+						target: section.find(".list")
+					});
+				}
 				break;
 		}
+
+		this.activeSection.removeClass("active");
+		this.activeSection = section.addClass("active");
+
+		window.body.css({
+			width: width +"px",
+			height: height +"px",
+		});
 	}
 };
 
