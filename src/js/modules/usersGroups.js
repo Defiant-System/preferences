c26db73ad4b038deb18e492395a683701de6d485
Hakan Bilgin <hbi99@hotmail.com>
2020-05-21 23:21:28 +0200

    users; more ui

--- src/js/modules/usersGroups.js
+++ src/js/modules/usersGroups.js
@@ -6,8 +6,16 @@
 		let self = parts.userGroups,
 			section = event.section,
 			el;
+		//console.log(event);
 		switch (event.type) {
 			case "init-view":
+				break;
+			case "select-user":
+				el = $(event.target);
+				if (el.hasClass("active") || el.hasClass("panel-left")) return;
+
+				el.parent().find(".active").removeClass("active");
+				el.addClass("active");
 				break;
 			case "unlock-view":
 				event.el.toggleClass("unlocked", event.el.hasClass("unlocked"));
