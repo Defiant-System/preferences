c2932092d0ef6263c9d18457f059570834791924
Hakan Bilgin <hbi99@hotmail.com>
2020-05-24 15:41:31 +0200

    ui; bugfix

--- src/js/main.js
+++ src/js/main.js
@@ -35,7 +35,7 @@ const preferences = {
 		this.setViewState();
 
 		// tmp
-		this.dispatch({ type: "go-to", view: "language" })
+		this.dispatch({ type: "go-to", view: "general" })
 	},
 	dispatch(event) {
 		let self = preferences,
