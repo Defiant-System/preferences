2b408049145aa1a4024f7a0b22a46876237fe8e4
Hakan Bilgin <hbi99@hotmail.com>
2021-03-04 18:24:22 +0100

    switch theme via general view

--- src/js/modules/general.js
+++ src/js/modules/general.js
@@ -9,6 +9,16 @@
 			case "init-view":
 				Self.section = event.section;
 				break;
+			case "select-ui-theme":
+				el = $(event.target);
+
+				// indicate active theme
+				el.parent().find(".active").removeClass("active");
+				el.addClass("active");
+
+				// execute shell command
+				defiant.shell(`sys -o ${el.data("arg")}`);
+				break;
 		}
 	}
 }
