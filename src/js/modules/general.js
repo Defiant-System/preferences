88301f498f5c48e0519fd548386d8296edaac158
Hakan Bilgin <hbi99@hotmail.com>
2021-04-25 12:54:59 +0200

    settings integration

--- src/js/modules/general.js
+++ src/js/modules/general.js
@@ -2,12 +2,26 @@
 // parts.general
 
 {
-	dispatch(event) {
+	async dispatch(event) {
 		let Self = parts.general,
+			shell,
+			value,
 			el;
 		switch (event.type) {
 			case "init-view":
 				Self.section = event.section;
+
+				// accent color
+				shell = await defiant.shell(`sys -j`);
+				el = Self.section.find(`.color-select_[data-color="${shell.result}"]`);
+				el.parent().find(".active").removeClass("active");
+				el.addClass("active");
+
+				// menubar toggle
+				shell = await defiant.shell(`sys -w`);
+				el = Self.section.find(`input[id="menubar-toggle"]`);
+				el.prop({ checked: shell.result });
+
 				break;
 			case "select-ui-theme":
 				el = $(event.target);
@@ -18,6 +32,25 @@
 
 				// execute shell command
 				defiant.shell(`sys -o ${el.data("arg")}`);
+				break;
+			case "select-accent-icon-color":
+				el = $(event.target);
+				if (!el.attr("style")) return;
+
+				// indicate active theme
+				el.parent().find(".active").removeClass("active");
+				el.addClass("active");
+
+				// execute shell command
+				value = el.attr("style").match(/(#.+);/)[1];
+				defiant.shell(`sys -j ${value}`);
+				break;
+			case "toggle-checkbox-value":
+				el = $(event.target);
+				if (el.attr("type") !== "checkbox") return;
+
+				// execute shell command
+				defiant.shell(`sys -w ${el.is(":checked")}`);
 				break;
 		}
 	}
